c2dbc1ef3fce4966306373bf3bdc92f0ba2c5f69
David <davideriksson@swedenmail.com>
2015-09-05 00:39:58 +0200

    travis and coveralls

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,16 +7,21 @@ require 'torrentify'
 program :version, '0.1'
 program :description, 'Terminal-client for webscraping torrent-sites'
 
-command :search do |c|
-  c.syntax = 'torrentify-cli search [options]'
-  c.summary = ''
-  c.description = ''
-  c.example 'description', 'command example'
-  c.option '--some-switch', 'Some switch that does something'
-  c.action do |args|
-    search_param = args.join(' ')
-    puts search_param
-    Torrentify.search(search_param)
-    # Do something or c.when_called Torrentify-cli::Commands::Search
+module Menu
+
+  def self.start
+    command :search do |c|
+      c.syntax = 'torrentify-cli search [options]'
+      c.summary = ''
+      c.description = ''
+      c.example 'description', 'command example'
+      c.option '--some-switch', 'Some switch that does something'
+      c.action do |args|
+        search_param = args.join(' ')
+        puts search_param
+        Torrentify.search(search_param)
+        # Do something or c.when_called Torrentify-cli::Commands::Search
+      end
+    end
   end
 end
