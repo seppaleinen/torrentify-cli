e9124d43b0b80866dc362092b15a50510284aa7a
David <davideriksson@swedenmail.com>
2015-10-04 22:36:35 +0200

    Added commander-documentations and ability to choose site to crawl

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,9 +7,9 @@ require 'yaml'
 
 # :name is optional, otherwise uses the basename of this executable
 class Menu
-  program :name, 'Foo Bar'
+  program :name, 'Torrentify'
   program :version, '1.0.0'
-  program :description, 'Stupid command that prints foo or bar.'
+  program :description, 'Interface for searching through torrentsites.'
   def initialize(torrentify = Torrentify)
     @torrentify = torrentify
   end
@@ -18,10 +18,16 @@ class Menu
 
   def start
     command :search do |c|
-      c.syntax = 'foobar foo'
-      c.description = 'Displays foo'
-      c.action do |args|
-        results = @torrentify.search args.join(' ')
+      c.syntax = 'search foo'
+      c.description = 'Search for torrents '
+      c.option '--search-engine PIRATEBAY', String, 'Search piratebay'
+      c.option '--search-engine ISOHUNT', String, 'Search isohunt'
+      c.option '--search-engine KICKASS', String, 'Search kickass'
+      c.option '--search-engine EXTRATORRENT', String, 'Search extratorrent'
+      c.option '--search-engine ALL', String, 'Default. Searches all sites'
+      c.action do |args, options|
+        options.default :search_engine => 'ALL'
+        results = @torrentify.search args.join(' '), options.search_engine
         results.each do |result|
           puts '---------------------------------'
           puts 'new search-engine'
@@ -34,8 +40,8 @@ class Menu
     end
 
     command :imdb do |c|
-      c.syntax = 'foobar foo'
-      c.description = 'Displays foo'
+      c.syntax = 'imdb userid'
+      c.description = 'Searches torrents for all movies on imdb watchlist'
       c.action do |args|
         imdb_user_id = ''
         if args.first.nil?
@@ -57,9 +63,8 @@ class Menu
       c.description = 'Display bar with optional prefix and suffix'
       c.option '--prefix STRING', String, 'Adds a prefix to bar'
       c.option '--suffix STRING', String, 'Adds a suffix to bar'
-      c.action do |options|
+      c.action do |_args, options|
         options.default :prefix => '(', :suffix => ')'
-        say args
         say "#{options.prefix}bar#{options.suffix}"
       end
     end
